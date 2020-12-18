8dab3b94d3acb14ee80a77b1a32fb547d3de5b1a
david <zein207@gmail.com>
2020-12-18 16:13:33 -0600

    Input Output

--- src/app/pages/grafica1/grafica1.component.ts
+++ src/app/pages/grafica1/grafica1.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-grafica1',
@@ -6,11 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class Grafica1Component implements OnInit {
+export class Grafica1Component {
 
-  constructor() { }
+  public labels1: string[] = ['Download Sales', 'In-Store Sales', 'Mail-Order Sales'];
+  public data1 = [
+    [330, 290, 376],
+  ];
 
-  ngOnInit(): void {
-  }
 
 }
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
-import { RouterModule } from '@angular/router'
+
+import { SharedModule } from '../shared/shared.module';
+import { ComponentsModule } from '../components/components.module';
+
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ProgressComponent } from './progress/progress.component';
 import { Grafica1Component } from './grafica1/grafica1.component';
 import { PagesComponent } from './pages.component';
-import { SharedModule } from '../shared/shared.module';
-
 
 
 
@@ -25,9 +28,11 @@ import { SharedModule } from '../shared/shared.module';
     PagesComponent
   ],
   imports: [
+    RouterModule,
+    FormsModule,
     CommonModule,
     SharedModule,
-    RouterModule
+    ComponentsModule,
   ]
 })
 export class PagesModule { }
